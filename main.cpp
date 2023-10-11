495eafa198cc0cc69cd74a879cc0b4f463638e9b
trangnguyen <tiffanyng202@gmail.com>
2023-10-10 22:40:56 -0400

    Insertion sort

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        // take x index i
+        int x = a[i], pos = i - 1;
+        while (pos >= 0 && x < a[pos]) {
+            a[pos + 1] = a[pos];
+            --pos;
+        }
+        a[pos + 1] = x;
+    }
+}
+
+int main() {
+    int a[1000];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    insertionSort(a, n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
